16427d42f49a62c791c386d54e6acf607996c574
GABIRO Jovial Fleuron <128985701+gabiro1@users.noreply.github.com>
2023-10-16 11:53:26 +0200

    Add files via upload

---
+++ SOCIALMEDIA/socialmediascript.js
@@ -0,0 +1,24 @@
+// const menuItem= document.querySelectorAll('.menu-item');
+
+// // remove active class to all item 
+
+// const changeActiveItem = () =>{
+//     menuItem.forEach(item =>{
+//         item.classList.remove('active');
+//     });
+// }
+// menuItem.forEach(item =>{
+//    item.addEventListener('click', ()=>{
+//     changeActiveItem();
+//     item.classList.add('active');
+//     if(item.id !='notification')
+//     {
+//         document.querySelector('.Notification-popups').
+//         style.display = 'none';
+//     }
+//     else{
+//         document.querySelector('.Notification-popups').
+//         style.display = 'block';
+//     }
+//    });
+// });
